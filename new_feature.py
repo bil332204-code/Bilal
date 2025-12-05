8fb3ad02a61bf652a1e9aab405378f8d1b8e1459
Bilal Ahmed <bil332204@gmail.com>
2025-12-05 16:14:06 +0500

    Added new feature file

---
+++ new_feature.py
@@ -0,0 +1,5 @@
+def subtract_numbers(a, b):
+    """Subtracts two numbers and returns the result."""
+    return a - b
+result = subtract_numbers(10, 4)
+print("The result of subtraction is:", result)
